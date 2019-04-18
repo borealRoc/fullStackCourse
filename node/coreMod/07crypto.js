cf5dd0361627c064ed8aa69b6cc3d5734e3c65f1
xusp <xusp@weizhenzhendeiMac-2.local>
2019-04-18 09:06:40 +0800

    ‘签名’

---
+++ node/coreMod/07crypto.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+
+let md5Obj = crypto.createHash('md5');
+let sha1Obj = crypto.createHash('sha1');
+
+md5Obj.update('123456')
+sha1Obj.update('123456')
+
+console.log(md5Obj.digest('hex'));
+console.log(sha1Obj.digest('hex'));
+
+// 双层md5
+function md5(str) {
+	let obj = crypto.createHash('md5');
+	obj.update(str);
+	return obj.digest('hex');
+}
+let nonce = 'd3jlk5f6'
+
+console.log(md5(md5('123456')) + nonce);
