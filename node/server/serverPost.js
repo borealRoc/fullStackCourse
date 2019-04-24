a6453366c4cf145081e4043bdaef1114a7989a31
xusp <xusp@weizhenzhendeiMac-2.local>
2019-04-24 09:11:52 +0800

    ‘增添注释’

--- node/server/serverPost.js
+++ node/server/serverPost.js
@@ -7,10 +7,12 @@ let server = http.createServer((req, res) => {
 	// 当有一块数据到达
 	req.on('data', data => {
 		str += data;
+		console.log('试一下get请求会不会触发这个事件1');
 	})
 	// 当全部数据到达
 	req.on('end', () => {
 		let post = querystring.parse(str)
+		console.log('试一下get请求会不会触发这个事件2');
 		console.log(str);
 		console.log(post);
 	})
