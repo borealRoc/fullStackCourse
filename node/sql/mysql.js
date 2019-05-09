6304255f4ddd504f8905cf7d28c8ca26c3643d81
xusp <xusp@weizhenzhendeiMac-2.local>
2019-05-09 10:48:42 +0800

    node中数据库基本操作

--- node/sql/mysql.js
+++ node/sql/mysql.js
@@ -1,22 +1,39 @@
 const mysql = require('mysql');
 
-let db = mysql.createConnection({
+let db = mysql.createPool({
 	host: 'localhost', 
 	port: 3306,
 	user: 'root',
 	password: 'QSXdr1991',
-	database: '20190429'
+	database: '20190429',
+	maxConnection: 10
 });
 // 增
-// db.query(`INSERT INTO user_data (ID, name, gender, chinese, math, english) VALUES (0, '小许', '男', 100, 100, 100);`, (err, data) => {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log(data);
-// 	}
-// })
+db.query(`INSERT INTO stu_data (ID, name, gender, chinese, math, english) VALUES (0, 'xu', '男', 100, 100, 100);`, (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+})
 // 删
-db.query(`DELETE FROM user_data WHERE ID = 1`, (err, data) => {
+db.query(`DELETE FROM stu_data WHERE ID = 5`, (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+})
+// 改
+db.query(`UPDATE stu_data SET chinese=5,math=99 WHERE ID = 2`, (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+})
+// 查
+db.query(`SELECT name, gender FROM stu_data WHERE ID = 3`, (err, data) => {
 	if (err) {
 		console.log(err);
 	} else {
