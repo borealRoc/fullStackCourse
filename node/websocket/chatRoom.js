75d9dfc615edff6f0675da63ce6abcd4030a1937
xusp <xusp@weizhenzhendeiMac-2.local>
2019-05-15 18:28:05 +0800

    简单聊天室功能

---
+++ node/websocket/chatRoom.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const io = require('socket.io');
+
+let httpServer = http.createServer();
+httpServer.listen(8080)
+
+let wsServer = io.listen(httpServer);
+let aSocket = [];
+wsServer.on("connection", socket => {
+
+	aSocket.push(socket);
+
+	socket.on('disconnect', () => {
+		let n = aSocket.indexOf(socket);
+		if (n !== -1) {
+			aSocket.splice(n, 1);
+		}
+	})
+
+	socket.on('msg', chatMes => {
+		aSocket.forEach( sock => {
+			if (sock !== socket) {
+				sock.emit('msg', chatMes);
+			}
+		})
+	})
+})
+
+setInterval(function () {
+	console.log(aSocket.length);
+}, 1000)
